6b0a7df7a5365cfece71417ca916b9bee9d5f1c9
HectorShin <hector.shin@usp.br>
2022-10-15 15:53:12 -0300

    Versão final sem componente reg

--- ep2/ep2a2.vhd
+++ ep2/ep2a2.vhd
@@ -19,35 +19,21 @@ entity regfile is
 end regfile;
 
 architecture regfile_arch of regfile is
-    component reg is
-        generic (
-            wordSize: natural := 4
-        );
-        port (
-            clock: in bit;
-            reset: in bit;
-            load: in bit;
-            d: in bit_vector(wordSize-1 downto 0);
-            q: out bit_vector(wordSize-1 downto 0)
-        );
-    end component;
     type register_data is array(0 to regn-1) of bit_vector(wordSize-1 downto 0);
-    signal registers_in : register_data;
-    signal registers_out : register_data;
+    signal registers : register_data;
     begin
-        registers_in(to_integer(unsigned(wr))) <= d;
-        q1 <= registers_out(to_integer(unsigned(rr1)));
-        q2 <= registers_out(to_integer(unsigned(rr2)));
-        register_file: for i in regn-1 downto 0 generate
-            last_register_if: if i = regn-1 generate
-                last_register_inst: reg 
-                    generic map(wordSize)
-                    port map(clock, reset, '0', registers_in(i), registers_out(i));
-            end generate;
-            others_registers: if i >= 0 generate
-                others_registers_inst: reg
-                    generic map(wordSize)
-                    port map(clock, reset, regWrite, registers_in(i), registers_out(i));
-            end generate;
-        end generate;
+        p0: process(clock, reset)
+        begin
+            if (reset = '1') then
+                for i in regn-1 downto 0 loop
+                    registers(i) <= (others => '0');
+                end loop;
+            elsif (clock'event) and (clock = '1') then
+                if (regWrite = '1') and (to_integer(unsigned(wr)) /= regn-1) then
+                    registers(to_integer(unsigned(wr))) <= d;
+                end if;
+            end if;
+        end process;
+        q1 <= registers(to_integer(unsigned(rr1)));
+        q2 <= registers(to_integer(unsigned(rr2)));
     end architecture;
