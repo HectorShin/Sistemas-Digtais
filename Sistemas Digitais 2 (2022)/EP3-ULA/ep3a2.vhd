9549550330ab049e07c44c7b4ffbff06033bbfcd
HectorShin <hector.shin@usp.br>
2022-11-13 12:52:19 -0300

    Envio final | Nota: 10

--- Sistemas Digitais 2 (2022)/EP3-ULA/ep3a2.vhd
+++ Sistemas Digitais 2 (2022)/EP3-ULA/ep3a2.vhd
@@ -1,55 +1,6 @@
 library ieee;
 use ieee.numeric_bit.all;
 
-entity fulladder is
-    port (
-      a, b, cin: in bit;
-      s, cout: out bit
-    );
-   end entity;
-
-architecture structural of fulladder is
-    signal axorb: bit;
-    begin
-        axorb <= a xor b;
-        s <= axorb xor cin;
-        cout <= (axorb and cin) or (a and b);
-    end architecture;
-
-library ieee;
-use ieee.numeric_bit.all;
-
-entity somador is
-    generic (
-        size : natural := 10 
-    );
-    port (
-        a, b : in bit_vector(size-1 downto 0);
-        cin: in bit;
-        S : out bit_vector (size-1 downto 0);
-        cout : out bit
-    );
-end entity;
-
-architecture somador_Arch of somador is
-    component fulladder
-        port (
-            a, b, cin: in bit;
-            s, cout: out bit
-        );
-    end component;
-    signal temp : bit_vector(size downto 0);
-    begin
-        temp(0) <= cin;
-        Cout <= temp(size);
-        FAA: for i in 0 to size-1 generate
-            FA_i : fulladder port map(a(i), b(i), temp(i), S(i),temp(i+1));
-        end generate;
-    end architecture;
-
-library ieee;
-use ieee.numeric_bit.all;
-
 entity alu is
     generic (
         size: natural := 10
@@ -65,53 +16,30 @@ entity alu is
 end entity alu;
 
 architecture alu_arch of alu is
-    component somador
-        generic (
-            size : natural := 10
-        );
+    component alu1bit
         port (
-            a, b : in bit_vector(size-1 downto 0);
-            cin: in bit;
-            S : out bit_vector (size-1 downto 0);
-            cout : out bit
+            a, b, less, cin: in bit;
+            result, cout, set, overflow: out bit;
+            ainvert, binvert: in bit;
+            operation: in bit_vector(1 downto 0)
         );
     end component;
-    signal s_and, s_or, s_a_tratado, s_b_tratado, s_fadd, s_nor, comparador,  s_comparador : bit_vector(size-1 downto 0);
-    signal resultado : bit_vector(size-1 downto 0);
-    signal zero : bit_vector(size-1 downto 0);
-    signal s_cin, s_cout: bit;
+    signal couts, sum, result : bit_vector(size-1 downto 0);
+    signal slt : bit;
+    signal zeros : bit_vector(size-1 downto 0);
     begin
-        s_a_tratado <= not(A) when S = "1100" else
-                       A;
-        s_b_tratado <= not(B) when S = "0110" or S = "1100" or S = "0111" else
-                       B;
-        s_cin <= '1' when S = "0110" or S = "1100" or S = "0111" else
-                 '0';
-        soma : somador generic map(size) port map(s_a_tratado, s_b_tratado, s_cin, s_fadd, s_cout);
-        zero <= (others=>'0');
-        s_and <= s_a_tratado and s_b_tratado;
-        s_or <= s_a_tratado or s_b_tratado;
-        s_nor <= s_a_tratado and s_b_tratado;
-        Co <= s_cout;
-        F <= resultado;
-        Z <= '1' when resultado = zero else '0';
-        --comparador_for: for i in size-2 downto 0 loop
-        --    comparador(i) <= '1' when A(i) = '1' and s_b_tratado(i) = '0' else
-        --                     '0';
-        --end generate;
-        --comparador(size-1) <= '0';
-        --s_comparador <= bit_vector(to_unsigned(1, size)) when (comparador = zero or (A(size-1) = '1' and s_b_tratado(size-1) = '0')) else
-        --                (others => '0');
-        s_comparador <= bit_vector(to_unsigned(1, size)) when (to_integer(signed(A)) < to_integer(signed(B))) else
-                        (others => '0');
-        with S select
-            resultado <= s_and when "0000",
-                         s_or when "0001",
-                         s_fadd when "0010",
-                         s_fadd when "0110",
-                         s_comparador when "0111",
-                         s_nor when "1100",
-                         zero when others;
-        Ov <= '1' when (s_a_tratado(size-1) = s_b_tratado(size-1) and s_fadd(size-1) = not(s_a_tratado(size-1))) else
-              '0';
+        alu_for : for i in size-1 downto 0 generate
+            alu_if : if i = 0 generate
+                lsb_alu : alu1bit port map (A(i), B(i), slt, S(2), result(i), couts(i), sum(i), open, S(3), S(2), S(1 downto 0));
+            elsif i = size-1 generate
+                msb_alu : alu1bit port map (A(i), B(i), '0', couts(i-1), result(i), Co, sum(i), Ov, S(3), S(2), S(1 downto 0));
+            elsif i > 0 and i < size-1 generate
+                othersb_alu : alu1bit port map (A(i), B(i), '0', couts(i-1), result(i), couts(i), sum(i), open, S(3), S(2), S(1 downto 0));
+            end generate;
+        end generate;
+        slt <= sum(size-1);
+        F <= result;
+        zeros <= (others => '0');
+        Z <= '1' when result = zeros else
+             '0';
     end architecture;
